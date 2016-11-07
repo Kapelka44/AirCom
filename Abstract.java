a62bf7e7d11b1e3f913d5c8753226294610e17b8
Maksim <danger2013@list.ru>
2016-11-07 14:01:06 +0300

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,29 @@
+package Avia;
+
+
+abstract class Abstract {
+    String name() {
+        return name();
+    }
+
+    int id() {
+        return id();
+    }
+
+    int weight() {
+        return weight();
+    }
+
+    int seats() {
+        return seats();
+    }
+
+    int fuel() {
+        return fuel();
+    }
+
+    int speed() {
+        return speed();
+    }
+
+}
